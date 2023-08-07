491fd1d09e33121ee31437a84010771eb3d23e78
Dan1Nguyen01 <98774378+Dan1Nguyen01@users.noreply.github.com>
2023-08-07 13:28:18 -0600

    try again

--- api/index.js
+++ api/index.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
-app.use("/images", express.static(path.join(__dirname, "images")));
+app.use("/images", express.static(path.join(__dirname, "/images")));
 
 app.use(
   cors({
