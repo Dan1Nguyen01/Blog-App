4977459b8ea995debcaa850d1969586d1bcf7b99
Dan1Nguyen01 <98774378+Dan1Nguyen01@users.noreply.github.com>
2023-08-07 01:50:55 -0600

    update

--- blog_app/src/App.js
+++ blog_app/src/App.js
@@ -18,7 +18,7 @@ const App = () => {
   return (
     <div>
       <UserContextProvider>
-        <HashRouter>
+        <BrowserRouter>
           <Navbar />
           <Routes>
             <Route index element={<Home />} />
@@ -29,7 +29,7 @@ const App = () => {
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
           </Routes>
-        </HashRouter>
+        </BrowserRouter>
       </UserContextProvider>
     </div>
   );
